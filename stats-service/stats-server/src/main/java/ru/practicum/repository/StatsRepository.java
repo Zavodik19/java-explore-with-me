5996af4c279fd9f71738ba47ade14bdc13f9ae8a
Zavodik19 <Zavodik19@yandex.ru>
2025-01-20 17:28:17 +0300

    добавлена сортировка по убыванию согласно ТЗ

--- stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -12,21 +12,25 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(*) ) " +
             "FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(*) DESC")
     List<ViewStatsDto> getAllHitsWithoutUris(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(*) ) " +
             "FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "AND h.uri IN :uris GROUP BY h.app, h.uri")
+            "AND h.uri IN :uris GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(*) DESC")
     List<ViewStatsDto> getAllHitsWithUris(List<String> uris, LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> getHitsWithoutUrisWithUniqueIp(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "AND h.uri IN :uris GROUP BY h.app, h.uri")
+            "AND h.uri IN :uris GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> getHitsWithUrisWithUniqueIp(List<String> uris, LocalDateTime start, LocalDateTime end);
 }
